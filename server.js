c00d51743f4e368ee0b2f9924fd0054e452762dc
Joshua Uyi <joshuaouyi@gmail.com>
2019-02-24 14:31:39 +0100

    configured for heroku

--- server.js
+++ server.js
@@ -17,11 +17,21 @@ app.get('/(:id)', function(req, res) {
 
 // get sharejs dependencies
 var sharejs = require('share');
-require('redis');
+
+// set up redis server
+var redisClient;
+console.log(process.env.REDISTOGO_URL);
+if (process.env.REDISTOGO_URL) {
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  redisClient = require("redis").createClient(rtg.port, rtg.hostname);
+  redisClient.auth(rtg.auth.split(":")[1]);
+} else {
+  redisClient = require("redis").createClient();
+}
 
 // options for sharejs 
 var options = {
-  db: {type: 'redis'},
+  db: {type: 'redis', client: redisClient}
 };
 
 // attach the express server to sharejs
